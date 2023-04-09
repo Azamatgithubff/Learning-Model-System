db6bd75786759308575e732f841409186a9c7966
Chyngyz <ryskeldiev_ch@iuca.kg>
2023-04-09 21:12:24 +0600

    JWT authentication

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 from courses.views import CourseAPIList, CourseAPIUpdate, CourseAPIDestroy
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/course/', CourseAPIList.as_view()),
     path('api/v1/course/<int:pk>/', CourseAPIUpdate.as_view()),
     path('api/v1/coursedelete/<int:pk>/', CourseAPIDestroy.as_view()),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
